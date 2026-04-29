90c10a9712e76744deafd10228ff10743de1e83b
local user <68118396+hemrajpal@users.noreply.github.com>
2026-04-29 13:13:31 +0530

    add role field with default user role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->appendToGroup('api', \App\Http\Middleware\ForceJsonResponse::class);
 
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
