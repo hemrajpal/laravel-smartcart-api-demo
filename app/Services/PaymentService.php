794d38ee7c41b876e8bdd44d398c10f903de9710
hemrajpal <68118396+hemrajpal@users.noreply.github.com>
2026-08-31 14:43:42 +0530

    Update PaymentService.php

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -1,1 +1,17 @@
-test
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+
+class PaymentService
+{
+    public function __construct(
+        protected $gateway
+    ) {}
+
+    public function createPayment(Order $order)
+    {
+        return $this->gateway->createPayment($order);
+    }
+}
