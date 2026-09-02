f58bab99a91d1a57c8c6ec827327b28505e603f0
hemrajpal <68118396+hemrajpal@users.noreply.github.com>
2026-09-02 11:07:43 +0530

    Update AdminOrderController.php

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -9,7 +9,7 @@ class AdminOrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = \App\Models\Order::with('payment')->latest()->paginate(10);
+        $orders = \App\Models\Order::with('paymentS')->latest()->paginate(10);
     
         return view('admin.orders.index', compact('orders'));
     }
