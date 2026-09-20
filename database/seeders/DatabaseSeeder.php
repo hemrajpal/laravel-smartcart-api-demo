f5852af8e219e321d1e971890693c58fb11d7406
Hemraj Pal <68118396+hemrajpal@users.noreply.github.com>
2026-09-20 17:15:00 +0530

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -48,5 +49,7 @@ class DatabaseSeeder extends Seeder
                 'role' => 'admin',
             ]
         );
+
+        Product::factory()->count(50)->create();
     }
 }
